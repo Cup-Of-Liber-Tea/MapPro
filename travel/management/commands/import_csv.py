d2f3dcb8d21b7431bc9a4f76b0ddfa5e6f005a0c
Cup-Of-Liber-Tea <hangyaku0955@naver.com>
2024-10-02 17:51:25 +0900

    승수님 테스트

--- travel/management/commands/import_csv.py
+++ travel/management/commands/import_csv.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
 from travel.models import Tourist
 import csv
+from django.core.files import File
+from django.conf import settings
+import os
 
 class Command(BaseCommand):
     help = 'Import tourist data from CSV file'
@@ -10,15 +13,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         csv_file_path = options['csv_file']
+        
         with open(csv_file_path, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                Tourist.objects.create(
-                    name=row['title'],
-                    description=row['descriptions'],
-                    location=row['location'],
-                    operating_hours=row['OPERATING_HOURS'],
-                    entrance_fee=row['ENTRANCE_FEE'],
-                    parking=row['PARKING'],
+                tourist = Tourist(
+                    id=row['Id'],
+                    name=row['Title'],
+                    description=row['Descriptions'],
+                    location=row['Location'],
+                    operating_hours=row['OPERATING HOURS'],
+                    entrance_fee=row['ENTRANCE FEE'],
+                    parking=row['PARKING']
                 )
+                
+                # 이미지 처리
+                if row['IMAGE']:
+                    image_path = os.path.join(settings.MEDIA_ROOT, row['IMAGE'])
+                    if os.path.exists(image_path):
+                        with open(image_path, 'rb') as img_file:
+                            tourist.image.save(row['IMAGE'], File(img_file), save=False)
+                
+                tourist.save()
+        
         self.stdout.write(self.style.SUCCESS('Successfully imported tourist data'))
