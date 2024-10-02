34a744f39d2a2d3dc1bc25810d319da3db1cc35c
Yuna Jang <yajoo8993@gmail.com>
2024-10-02 20:00:35 +0900

    'author' 삭제

--- user/forms.py
+++ user/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['author', 'title', 'content', 'location', 'image']
+        fields = ['title', 'content', 'location', 'image']
